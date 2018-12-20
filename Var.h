b3fbfc4dd22542bf3e5cc05f70cc4686ef694a5d
Tal Levy <orntal17@gmail.com>
2018-12-20 16:57:39 +0200

    Var supports assigns now.

--- Var.h
+++ Var.h
@@ -23,6 +23,24 @@ public:
     Var(string varName, double val);
 
     /**
+     * CTOR.
+     * @param varName string name
+     */
+    Var(string varName);
+
+    /**
+     * assign a value to the Var, shall be used by the assign command at run-time.
+     * @param val
+     */
+    void assignValue(double val);
+
+    /**
+     * assign a value of an Expression to the var, shall be used by the assign command.
+     * @param ex
+     */
+    void assignValue(Expression* ex);
+
+    /**
      * calculates the value of the node.
      * @return double value.
      */
