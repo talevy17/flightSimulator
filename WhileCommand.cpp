78580c2028e5645d7f1a6b9503807f6b90bd8877
Tal Levy <orntal17@gmail.com>
2018-12-21 21:47:43 +0200

    introducing while commands ! (Not throughly checked yet)

--- WhileCommand.cpp
+++ WhileCommand.cpp
@@ -3,3 +3,53 @@
 //
 
 #include "WhileCommand.h"
+
+/**
+* CTOR, initializes the data a command maps in super.
+* @param variables
+* @param commandMap
+*/
+WhileCommand::WhileCommand(FlightDataVariables *variables, map<string, Command *> *commandMap) {
+    this->setSymbolTable(variables);
+    this->setCommandMap(commandMap);
+}
+
+/**
+* while the condition is true, execute the commands inside the block.
+* @param it iterator.
+*/
+void WhileCommand::execute(vector<string>::iterator &it) {
+    //keep locally the commands and arguments in the block.
+    vector<Command *> cmds;
+    vector<vector<string>::iterator> args;
+    Expression *condition;
+    //try parsing the condition.
+    try {
+        this->parseCondition(++it, condition);
+    } catch (const char *e) {
+        throw e;
+    }
+    //check the condition.
+    if (condition->calculate()) {
+        //try parsing the commands.
+        try {
+            this->parseCommands(it, cmds, args);
+        } catch (const char *e) {
+            throw e;
+        }
+    } else {
+        return;
+    }
+    while (condition->calculate()) {
+        //try to execute all of the commands in the block.
+        for (unsigned long int i = 0; i < cmds.size(); i++) {
+            auto temp = args.at(i);
+            try {
+                cmds.at(i)->execute(temp);
+            } catch (const char *e) {
+                throw e;
+            }
+        }
+    }
+    delete (condition);
+}
--- WhileCommand.h
+++ WhileCommand.h
@@ -5,9 +5,25 @@
 #ifndef FLIGHTSIMULATOR_WHILECOMMAND_H
 #define FLIGHTSIMULATOR_WHILECOMMAND_H
 
+#include "ConditionCommand.h"
 
-class WhileCommand {
+/**
+ * implements the condition block while loop, as part of the design pattern.
+ */
+class WhileCommand : public ConditionCommand {
+public:
+    /**
+     * CTOR, initializes the data a command maps in super.
+     * @param variables
+     * @param commandMap
+     */
+    WhileCommand(FlightDataVariables *variables, map<string, Command *> *commandMap);
 
+    /**
+     * while the condition is true, execute the commands inside the block.
+     * @param it iterator.
+     */
+    void execute(vector<string>::iterator &it);
 };
 
 
